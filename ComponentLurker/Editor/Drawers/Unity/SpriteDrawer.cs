6aadec4c06adfa3f1e34b3cc281cc379e46a1a0f
vksokolov <vladislav.k.sokolov@gmail.com>
2023-09-23 19:59:41 +0300

    UnityObjectDrawer
    Added a UnityObjectDrawer as a base drawer for UnityEngine.Object-derived types. Now it is easier to add Unity-related drawers.

--- ComponentLurker/Editor/Drawers/Unity/SpriteDrawer.cs
+++ ComponentLurker/Editor/Drawers/Unity/SpriteDrawer.cs
@@ -4,36 +4,14 @@ using UnityEngine;
 
 namespace ComponentLurker.Drawers
 {
-    public class SpriteDrawer : BaseDrawer<Sprite>
+    public class SpriteDrawer : UnityObjectDrawer<Sprite>
     {
-        public override ComparisonOperations AllowedOperations => 
-            ComparisonOperations.Equal | 
-            ComparisonOperations.NotEqual;
-        
-        private static readonly Func<Sprite, GUILayoutOption[], Sprite> DrawFunc = (value, options) =>
-        {
-            var sprite = EditorGUILayout.ObjectField(value, typeof(Sprite), false, options) as Sprite;
-            return sprite;
-        };
-        
-        private static readonly Func<string, Sprite, GUILayoutOption[], Sprite> DrawFuncWithLabel = (label, value, options) =>
-        {
-            var sprite = EditorGUILayout.ObjectField(label, value, typeof(Sprite), false, options) as Sprite;
-            return sprite;
-        };
-        
-        public SpriteDrawer(object value, GUILayoutOption[] options) : base(value, options, DrawFunc)
+        public SpriteDrawer(object value, GUILayoutOption[] options) : base(value, options)
         {
         }
 
-        public SpriteDrawer(string label, object value, GUILayoutOption[] options) : base(label, value, options, DrawFuncWithLabel)
+        public SpriteDrawer(string label, object value, GUILayoutOption[] options) : base(label, value, options)
         {
         }
-
-        protected override bool IsEqual(object value) => 
-            (UnityEngine.Object)value == (UnityEngine.Object)Value;
-
-        protected override bool IsNotEqual(object value) => 
-            !(UnityEngine.Object)value == (UnityEngine.Object)Value;
     }
 }
